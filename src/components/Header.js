f4f5c5fab88b9b355e340413f62d74ccf84ffced
Wojteksiwek <wojteksiwek12@wp.pl>
2019-05-10 13:37:15 +0200

    add animation in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 import './Header.css';
+import $ from 'jquery';
 
 class Header extends Component {
+
+    handleBackground = () => {
+        $('#idHeader').css({
+            'opacity': 2*(1-$(document).scrollTop()/$('#idHeader').outerHeight())
+        });
+    }
+
+
+    componentDidMount(){
+        $(document).on("scroll",this.handleBackground);
+    }
+    
     render(){
         return(
-        <div className="Header">
-            <img src="image1.jpg" alt="domki"/>
+        <div className="Header" id='idHeader'>
             <div className="title">
                 <p id="firstline">Witaj na</p>
                 <p id="secondline">domki letniskowe</p>
